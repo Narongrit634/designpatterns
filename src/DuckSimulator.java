1417e5a83585983a75d0f5cc66bfdeef62fa4059
Narongrit Neewong <naonrgirt.n@ku.th>
2019-09-15 20:15:27 +0700

    add duck

---
+++ src/DuckSimulator.java
@@ -0,0 +1,22 @@
+public class DuckSimulator {
+    public static void main(String[] args) {
+        DuckSimulator simulator = new DuckSimulator();
+        simulator.simulate();
+    }
+    void  simulate() {
+        Quackable mallardDuck = new MallardDuck();
+        Quackable readHeadDuck = new RedHeadDuck();
+        Quackable duckCall = new DuckCall();
+        Quackable rubberDuck = new RubberDuck();
+
+        System.out.println("\nDuck Simulator");
+
+        simulate(mallardDuck);
+        simulate(readHeadDuck);
+        simulate(duckCall);
+        simulate(rubberDuck);
+    }
+    void simulate(Quackable duck) {
+        duck.quack();
+    }
+}
